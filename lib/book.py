9a27b1b093e29b83f04d8848b64a331cb4eb0767
Njugunasam <sammynjuguna5826@gmail.com>
2023-12-17 14:00:10 +0300

    done

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 class Book:
+    def __init__(self, title, pages):
+        self.title = title
+        self.page_count = pages
+
     pass
-    
-        
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
 
 class Shoe:
+    def __init__(self, brand, size, color):
+        self.brand = brand
+        self.size = size
+        self.color = color
     pass
